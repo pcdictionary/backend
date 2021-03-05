fa12fc6e802da4640d4ac06126c9372633dfd711
Phurba Sherpa <srpphurba@gmail.com>
2021-03-05 13:16:45 -0500

    updated modelsv3

--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
-import pkg from '@prisma/client';
+import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
-import  express  from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 const prisma = new PrismaClient();
 const typeDefs = `
   type User {
     email: String!
     name: String
+  }'
+  type Item{
+
   }
   type Query {
     allUsers: [User!]!
+    allItems : [I]
   }
 `;
 const resolvers = {
   Query: {
     allUsers: () => {
       return prisma.user.findMany();
-    }
-  }
+    },
+  },
 };
 export const schema = makeExecutableSchema({
   resolvers,
   typeDefs,
 });
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 app.listen(4000);
