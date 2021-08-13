9b34aa89956fffa941b54ae4a18122ffac5a07b9
Don <itizidon@gmail.com>
2021-08-13 13:13:45 -0400

    updated K value

--- src/utils/elo.js
+++ src/utils/elo.js
@@ -1,10 +1,10 @@
 export const probability = (rating1, rating2) => {
   return (
-    (1.0 * 1.0) / (1 + 1.0 * Math.pow(10, (1.0 * (rating1 - rating2)) / 400))
+    (1.0 * 1.0) / (1 + 1.0 * Math.pow(10, (1.0 * (rating1 - rating2)) / 400000))
   );
 };
 
-export const EloRating = (Ra, Rb, K = 30, d = 1) => {
+export const EloRating = (Ra, Rb, K = 30000, d = 1) => {
   const Pa = probability(Rb, Ra);
 
   if (d === 1) {
